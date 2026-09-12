3e8edcd2e9110724c4d53220c320ebffb2c65edc
Bushidoxyz <mutlushenolov@gmail.com>
2026-09-12 15:57:09 +0300

    fix: logo adresleri Bushidoxyz/My reposuna yonlendirildi ve surum v29 yapildi

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
 }
 
 // Sürüm numarasını tam sayı (Integer) olarak belirtiyoruz
-version = 28
+version = 29
 
 cloudstream {
     // Eklentimizin uygulamanın uzantılar sayfasında görünecek bilgileri
--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -22,8 +22,7 @@ class ExampleProvider : MainAPI() {
         Pair("kids", "Çocuk Kanalları")
     )
 
-    private val defaultPoster = "https://www.seirsanduk.online/images/logo.png"
-    private val rawLogoBase = "https://raw.githubusercontent.com/Bushidotv/Bushido/master/logos_hd"
+    private val rawLogoBase = "https://raw.githubusercontent.com/Bushidoxyz/My/master/logos_hd"
 
     private val browserHeaders = mapOf(
         "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
